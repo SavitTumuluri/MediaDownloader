1695612fe829c483e569f063154fa68b4daeb0da
journeyhancock <journeyhancock480@gmail.com>
2024-09-21 03:00:53 -0700

    homepage link entry

--- mediawebsite/src/home.js
+++ mediawebsite/src/home.js
@@ -7,9 +7,16 @@ function Home() {
       <header className="App-header">
         <div className="App-popup">
           <h1>Welcome to MediaConverter Home</h1>
-          <button className='button_ui'>
+          {/* <button className='button_ui'>
             Enter
-          </button>
+          </button> */}
+          <label for="options"></label>
+          <select id="options" className='link_submit'>
+            <option value="" disabled selected>Select</option>
+            <option value="Youtube">Youtube</option>
+          </select>
+          <input id="url" type="text" className='link_input'></input>
+          <input type="submit" value="Convert" className='link_submit'></input>
         </div>
       </header>
     </div>
