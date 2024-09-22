4cf640aab952c66f587fbc961ea29d38c6d34556
SavitTumuluri <savit.s.tumuluri@gmail.com>
2024-09-21 20:20:21 -0400

    temp

---
+++ mediawebsite/src/CustomSelect.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './CustomSelect.css';
+
+const options = [
+  { value: 'Youtube', label: 'YouTube', icon: '/youtube_logo.png' }
+];
+
+function CustomSelect({ onChange }) {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    onChange(option);
+  };
+
+  return (
+    <div className="custom-select">
+      <div className="selected-item">
+        {selectedOption ? (
+          <>
+            <img src={process.env.PUBLIC_URL + selectedOption.icon} alt="" className="option-icon" />
+            {selectedOption.label}
+          </>
+        ) : (
+          'Select'
+        )}
+      </div>
+      <div className="options">
+        {options.map((option) => (
+          <div
+            key={option.value}
+            className="option-item"
+            onClick={() => handleOptionClick(option)}
+          >
+            <img src={process.env.PUBLIC_URL + option.icon} alt="" className="option-icon" />
+            {option.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CustomSelect;
--- mediawebsite/src/home.js
+++ mediawebsite/src/home.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
+import CustomSelect from './CustomSelect';
 
 function Home() {
+  const handleSelectChange = (option) => {
+    console.log('Selected option:', option);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-popup">
           <h1>Welcome to MediaConverter Home</h1>
-          {/* <button className='button_ui'>
-            Enter
-          </button> */}
-          <label for="options"></label>
-          <select id="options" className='link_submit'>
+          <label htmlFor="options"></label>
+          {/*<select id="options" className='link_submit'>
             <option value="" disabled selected>Select</option>
             <option value="Youtube">Youtube</option>
-          </select>
+          </select>*/}
+          <CustomSelect onChange={handleSelectChange} />
           <input id="url" type="text" className='link_input'></input>
           <input type="submit" value="Convert" className='link_submit'></input>
         </div>
